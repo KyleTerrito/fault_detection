b48ded7ffb6bb8f8007e8172abaaff01abeed8de
Miriam Nnadili <mnnadi1@lsu.edu>
2022-03-07 11:56:00 -0600

    Update dataPreprocessing.py

--- src/dataPreprocessing.py
+++ src/dataPreprocessing.py
@@ -6,3 +6,24 @@ Is the interfact with data.
 Provides methods for data preprocessing for faulDetection.
 
 """
+#import packages
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+
+class DataPreprocessing():
+    def __init__(self):
+        pass
+
+    def load_data(self):
+        #Load data
+        data = pd.read_excel("data", header=None).dropna()
+        print(data.head(10))
+
+        data = data.to_numpy()
+
+        return data
+
+    #scaling
+    scaler = StandardScaler()
+    data = scaler.fit_transform(data)
