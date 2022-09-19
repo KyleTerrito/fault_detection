dfe2f601cfb83428d22e4851a903d54a311e4827
lbrice1 <62967586+lbrice1@users.noreply.github.com>
2022-09-19 16:55:55 -0500

    updated opt_full_tep test

--- tests/opt_full_tep.py
+++ tests/opt_full_tep.py
@@ -1,15 +1,33 @@
+import sys
+import os
+
+os.chdir('../')
+os.getcwd()
+
 from autocluster.autoKNN import AutoKNN
 
 
 def test():
     knn = AutoKNN()
-    knn.get_data(path='data/processed/Full_TEP.xlsx', exp='fulltep')
+    knn.get_data(path=r'C:\\Users\\lbrice1\\Documents\\GitHub Repos\\fault_detection\\data\\processed\\Full_TEP.xlsx', exp='fulltep')
 
-    dr_methods = ['NO DR', 'PCA', 'UMAP']
-    cl_methods = ['KMEANS', 'DBSCAN', 'HDBSCAN']
+    dr_methods = [
+                    'NO DR', 
+                    #'PCA', 
+                    #'UMAP'
+                ]
 
-    knn.optimize(dr_methods, cl_methods, termination='run')
+    cl_methods = [
+                    'KMEANS', 
+                    #'DBSCAN', 
+                    #'HDBSCAN'
+                ]
 
-    knn.show_solutions()
+    res_dict = knn.optimize(dr_methods, cl_methods, termination='test')
+
+    print(res_dict)
+    #knn.show_solutions()
 
     return None
+
+test()
